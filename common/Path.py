573528e5149300df8a6db39e5135db0188f1d79f
SailFan <m17600576201@163.com>
2019-01-16 00:36:18 +0800

    openChrome and screent

--- common/Path.py
+++ common/Path.py
@@ -3,5 +3,9 @@ import os
 BASE_PATH = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
 CONFIG_YAML_PATH = os.path.join(BASE_PATH, "config", "config.yaml")
 DRIVERS_PATH = os.path.join(BASE_PATH, "drivers")
+REPORT_PATH = os.path.join(BASE_PATH,"report")
 
+
+if __name__ == '__main__':
+    print(REPORT_PATH)
 
--- common/change_open_browser.py
+++ common/change_open_browser.py
@@ -1,7 +1,7 @@
 import time
 import os
 from selenium import webdriver
-from common.Path import DRIVERS_PATH
+from common.Path import DRIVERS_PATH,REPORT_PATH
 DRIVERS_CHROME_PATH = os.path.join(DRIVERS_PATH,"chromedriver.exe")
 DRIVERS_FIREFOX_PATH = os.path.join(DRIVERS_PATH,"IEDriverServer.exe")
 DRIVERS_PHANTOMJS_PATH = os.path.join(DRIVERS_PATH,"phantomjs.exe")
@@ -20,3 +20,30 @@ class Browers():
         else:
             raise UnSupportBrowserTypeError('仅支持%s!' % ', '.join(TYPES.keys()))
         self.driver = None
+    def get(self, url, maxmize_window = True, implicitly_wait=30):
+        self.driver = self.browser(EXECUTABLE_PATH[self._type])
+        self.driver.get(url)
+        if maxmize_window:
+            self.driver.maximize_window()
+        self.driver.implicitly_wait(implicitly_wait)
+        return self
+    def savaScreenShot(self,name="screenShot"):
+        dayName = time.strftime("%Y%m%d", time.localtime(time.time()))
+        print(dayName)
+        screenshotPath = REPORT_PATH+'\screenshot_%s' %dayName
+        print(screenshotPath+"-------------------------------------")
+        print(REPORT_PATH+"----------------------------------")
+        if not os.path.exists(screenshotPath):
+            os.makedirs(screenshotPath)
+        tmImage = time.strftime("%H%M%S", time.localtime(time.time()))
+        screenShot = self.driver.save_screenshot(screenshotPath.join(name).join(tmImage))
+        return  screenShot
+    def close(self):
+        self.driver.close()
+
+    def quit(self):
+        self.driver.quit()
+if __name__ == '__main__':
+    b = Browers("chrome").get("http://www.baidu.com")
+    b.savaScreenShot("lww")
+    b.quit()
