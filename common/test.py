ef9d8a621fd994023704c6bd01fba7c094b1972c
SailFan <m17600576201@163.com>
2019-01-20 01:44:52 +0800

    openChrome and screent

--- common/test.py
+++ common/test.py
@@ -1,48 +1,38 @@
-import time
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-driver = webdriver.Chrome("D:\pycode\Test_Cms\drivers\chromedriver.exe")
-driver.get("https://www.baidu.com/")
-# driver.current_window_handle
-# title = driver.title
-# url = driver.current_url
-# driver.execute()
-# driver.execute_script()
-# driver.window_handles
-# driver.current_url
-# driver.find_element_by_css_selector()
-driver.find_element_by_id("kw").send_keys("python")
-# driver.switch_to.frame()
-# driver.switch_to.window(other_window)
-# ActionChains(driver).move_to_element(to_element).perform()
-# print(url)
-# a = "http://www.baidu.com";
-# b = "http://www.163.com"
-# print(a is b)
-# print(title)
-time.sleep(3)
-driver.quit()
-# class FooParent(object):
-#     def __init__(self):
-#         self.parent = 'I\'m the parent.'
-#         print('Parent')
+from selenium.webdriver.common.by import By
+
+# 1 初始化浏览器对象
+browser = webdriver.Chrome()
+#browser = webdriver.Firefox()
+#browser = webdriver.Edge()
+#browser = webdriver.PhantomJS()
+#browser = webdriver.Safari()
+
+#2 访问界面
+
+url = "https://mail.163.com/"
+browser.get(url)
+
+# 3元素获取
+# browser.find_element_by_id()#根据id获取
+# browser.find_element_by_xpath()#根据xpath获取
+# browser.find_elements_by_css_selector()#根据css选择器获取
+# browser.find_element_by_class_name()#根据css获取
+# browser.find_element_by_name()#根据name获取
+# browser.find_element_by_link_text()#根据link精确定位
+# browser.find_element_by_partial_link_text()#根据link模糊定位
+# browser.find_element_by_tag_name()#根据tag_name定位元素
+
+# 通用方法,需要传入两个参数， 查找方法和值， 结果与以上八种完全一致， 不过会用起来更灵活
+# browser.find_element(By.xxx, "")
 #
-#     def bar(self, message):
-#         print("%s from Parent" % message)
-#
-#
-# class FooChild(FooParent):
-#     def __init__(self):
-#         # super(FooChild,self) 首先找到 FooChild 的父类（就是类 FooParent），然后把类B的对象 FooChild 转换为类 FooParent 的对象
-#         super(FooChild, self).__init__()
-#         print('Child')
-#
-#     def bar(self, message):
-#         super(FooChild, self).bar(message)
-#         print('Child bar fuction')
-#         print(self.parent)
-#
-#
-# if __name__ == '__main__':
-#     fooChild = FooChild()
-#     fooChild.bar('HelloWorld')
+# 如果查找的元素在页面中是单个， 那么用以上方法就可以， 如果查找的元素有多个， 可以用browser.find_elements(),多了一个s, 如果还用browser.find_elements()查找
+# 将只会得到第一个元素
+print(browser.current_url)
+
+userNameInput = browser.find_element(By.CSS_SELECTOR, ".u-input box input:first-child")
+# passWordInput = browser.find_element(By.CSS_SELECTOR, "input[type='password']")
+# submitButton = browser.find_element(By.CSS_SELECTOR, "a[id='dologin']")
+userNameInput.send_keys("m17600576201")
+# passWordInput.send_keys("ww6201")
+# submitButton.click()
